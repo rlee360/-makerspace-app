28465b914e393065ea2a7f502281dd99a017c86a
rlee360 <Lee66@cooper.edu>
2021-12-02 02:16:31 -0500

    changed node project init

--- makerspace-frontend/src/App.vue
+++ makerspace-frontend/src/App.vue
@@ -1,85 +1,28 @@
 <template>
-  <div class="container mt-5 mb-5">
-    <div class="row">
-      <div class="col mx-2 px-2 py-3 bg-light border rounded">
-        <h6>New Jobs</h6>
-        <draggable class="draggable-list" :list="tasks.jobs" group="tasks">
-          <div v-for="(idea, i) in tasks.jobs" :key="i">
-            <div class="bg-white mt-3 p-2 shadow border rounded">
-              <p>{{ idea }}</p>
-            </div>
-          </div>
-        </draggable>
-      </div>
-      <div class="col">
-        <div class="col mx-2 px-2 py-3 bg-light border rounded">
-          <h6>Printer 1</h6>
-          <draggable class="draggable-list" :list="tasks.printer1" group="tasks">
-            <div v-for="(job, i) in tasks.printer1" :key="i">
-              <div class="bg-white mt-3 p-2 shadow border rounded">
-                <p>{{ job }}</p>
-              </div>
-            </div>
-          </draggable>
-        </div>
-        <div class="col mx-2 px-2 py-3 bg-light border rounded">
-          <h6>Printer 2</h6>
-          <draggable class="draggable-list" :list="tasks.printer2" group="tasks">
-            <div v-for="(job, i) in tasks.printer2" :key="i">
-              <div class="bg-white mt-3 p-2 shadow border rounded">
-                <p>{{ job }}</p>
-              </div>
-            </div>
-          </draggable>
-        </div>
-      </div>
-      <div class="col">
-        <div class="col mx-2 px-2 py-3 bg-light border rounded">
-          <h6>Completed</h6>
-          <draggable class="draggable-list" :list="tasks.completed" group="tasks">
-            <div v-for="(task, i) in tasks.completed" :key="i">
-              <div class="bg-white mt-3 p-2 shadow border rounded">
-                <p>{{ task }}</p>
-              </div>
-            </div>
-          </draggable>
-        </div>
-      </div>
-    </div>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import draggable from "vuedraggable";
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
+  name: 'App',
   components: {
-    draggable,
-  },
-  data() {
-    return {
-      tasks: {
-        jobs: ["camera_stand.stl", "rocktopus.stl", "flange.stl"],
-        printer1: ["iris.stl", "gyroid_dissected.stl"],
-        printer2: ["aerospike_nozzle.stl", "brain_model.stl"],
-        completed: [],
-      },
-    };
-  },
-};
+    HelloWorld
+  }
+}
 </script>
 
-<style scoped>
-h6 {
-  font-weight: 700;
-}
-.col {
-  height: 90vh;
-  overflow: auto;
-}
-.draggable-list {
-  min-height: 10vh;
-}
-.draggable-list > div {
-  cursor: pointer;
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>

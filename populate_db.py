01a0c08b1c3d9c36cf45c947b8d56b5452acded6
rlee360 <Lee66@cooper.edu>
2021-12-10 10:24:22 -0500

    finished machines, added sample doc for each collection

--- populate_db.py
+++ populate_db.py
@@ -13,23 +13,6 @@ Try adding mimetype checks to the upload_file func
 Implement all remaining unpopulated tables
 """
 
-def insert_db(db_host, db_name, col_name, docs_list, update_field=None):
-    client = pymongo.MongoClient(db_host)
-    db = client[db_name]
-    col = db[col_name]
-    col.insert_many(docs_list)
-    # used to skip repeat items and save some time if there is a unique id to search with
-    # for doc in docs_list:
-        # col.update_one(filter={update_field : doc[update_field]}, update={"$set" : doc}, upsert=True)
-
-machine_doc = {
-    "_id" : "FDM 1",
-    "brand" : "Prusa",
-    "model" : "i3 MK3",
-    "build_volume" : [210, 210, 250],
-    "maintenance" : False,
-    "jobs" : []
-}
 # link is not required because it might not be applicable (i.e. bought in store, at event, etc.)
 material_schema = {
     "title": "material",
@@ -40,7 +23,7 @@ material_schema = {
         "color",
         "brand",
         "grams_remaining",
-        "valid_printers"
+        "valid_machines"
     ],
     "properties": {
         "name": {"bsonType": "string"},
@@ -48,21 +31,10 @@ material_schema = {
         "material": {"bsonType": "string"},
         "color": {"bsonType": "string"},
         "brand": {"bsonType": "string"},
+        "grams_remaining": {"bsonType": "number"},
         "link" : {"bsonType": "string"},
-        "grams_remaining" : {"bsonType": "number"},
-        "valid_printers": {"bsonType": "array"}
+        "valid_machines": {"bsonType": ["array"]}
     }
-}
-
-material_doc = {
-    "name" : "Black PLA+",
-    "type" : "filament",
-    "material" : "PLA",
-    "color" : "black",
-    "brand" : "Inland",
-    "link" : "https://www.microcenter.com/product/632388/inland-175mm-black-pla-pro-3d-printer-filament-1kg-spool-(22-lbs)",
-    "grams_remaining" : 400,
-    "valid_printers" : []
 }
 
 job_schema = {
@@ -92,18 +64,6 @@ job_schema = {
     }
 }
 
-job_doc = {
-    "name": "benchy.stl",
-    "email": "test@cooper.edu",
-    "material": "",  # this relies on diff materials being available
-    "shells": 2,
-    "infill": 20,
-    "top_bottom": 4,
-    "notes": "",
-    "status": "inactive",
-    "machine": machine_doc["_id"]
-}
-
 operator_schema = {
     "title": "operator",
     "required": [
@@ -122,6 +82,50 @@ operator_schema = {
     }
 }
 
+machine_schema = {
+    "title": "machine",
+    "required": [
+        "_id",
+        "type",
+        "brand",
+        "model",
+        "build_volume",
+        "maintenance"
+    ],
+    "properties": {
+        "_id": {"bsonType": "string"},
+        "type": {"bsonType": "string"},
+        "brand": {"bsonType": "string"},
+        "build_volume": {"bsonType": "array"},
+        "maintenance": {"bsonType": "bool"},
+        "notes": {"bsonType": "array"},
+        "jobs": {"bsonType": "array"},
+    }
+}
+
+material_doc = {
+    "name" : "Black PLA+",
+    "type" : "filament",
+    "material" : "PLA",
+    "color" : "black",
+    "brand" : "Inland",
+    "grams_remaining" : 300,
+    "link" : "https://www.microcenter.com/product/632388/inland-175mm-black-pla-pro-3d-printer-filament-1kg-spool-(22-lbs)",
+    "valid_machines" : ["FDM 1", "FDM 2", "FDM 3", "FDM 4", "FDM 5", "FDM 6"]
+}
+
+job_doc = {
+    "name": "benchy.stl",
+    "email": "test@cooper.edu",
+    "material": "",  # this relies on diff materials being available
+    "shells": 2,
+    "infill": 20,
+    "top_bottom": 4,
+    "notes": "",
+    "status": "inactive",
+    "machine": "FDM 1"
+}
+
 operator_doc = {
         "name": "John Smith",
         "email": "test_operator@cooper.edu",
@@ -129,6 +133,43 @@ operator_doc = {
         "wage": 15.75,
         "hours": 2.5
 }
+
+machine_doc = {
+    "_id" : "FDM 1",
+    "type" : "FDM",
+    "brand" : "Prusa",
+    "model" : "i3 MK3",
+    "build_volume" : [210, 210, 250],
+    "maintenance" : False,
+    "notes" : [],
+    "jobs" : []
+}
+
+machine_docs = [
+    ["FDM 1", "FDM", "Prusa", "i3 MK3", [210, 210, 250], False, [], []],
+    ["FDM 2", "FDM", "Prusa", "i3 MK3", [210, 210, 250], False, [], []],
+    ["FDM 3", "FDM", "Prusa", "i3 MK3", [210, 210, 250], False, [], []],
+    ["FDM 4", "FDM", "Prusa", "i3 MK3", [210, 210, 250], True, ["x belt is too loose."], []],
+    ["FDM 5", "FDM", "Prusa", "i3 MK3", [210, 210, 250], False, [], []],
+    ["FDM 6", "FDM", "Prusa", "i3 MK3", [210, 210, 250], False, [], []],
+    ["FDM 7", "FDM", "Tinkerine", "Ditto Pro", [215, 165, 220], True, ["bed needs to be leveled", "nozzle is clogged"], []],
+
+    ["SLA 1", "SLA", "Elegoo", "Mars", [120, 68, 155], False, [], []],
+
+    ["Laser 1", "Laser", "Glowforge", "Pro", [304, 518, 0.6], False, [], []]
+]
+
+def machines_list_to_dict(doc):
+    return {
+        "_id": doc[0],
+        "type": doc[1],
+        "brand": doc[2],
+        "model": doc[3],
+        "build_volume": doc[4],
+        "maintenance": doc[5],
+        "notes": doc[6],
+        "jobs": doc[7]
+    }
 
 def create_col_schema(db, col_name, schema):
     res = db.create_collection(col_name, validator={"$jsonSchema": schema})
@@ -144,9 +185,14 @@ def main():
     create_col_schema(db, "materials", material_schema)
     create_col_schema(db, "jobs", job_schema)
     create_col_schema(db, "operators", operator_schema)
+    create_col_schema(db, "machines", machine_schema)
     db["materials"].insert_one(material_doc)
     db["jobs"].insert_one(job_doc)
     db["operators"].insert_one(operator_doc)
+
+    machine_docs_dicts = map(machines_list_to_dict, machine_docs)
+
+    db["machines"].insert_many(machine_docs_dicts)
 
 
 if __name__ == "__main__":

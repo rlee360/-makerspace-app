e2eb4c0cac924ca0bfacbcdb787cfc28b93bdbba
rlee360 <Lee66@cooper.edu>
2021-12-10 09:45:45 -0500

    added operators

--- populate_db.py
+++ populate_db.py
@@ -1,8 +1,11 @@
 import os
+
+import bson
 import pymongo
 from bson.son import SON
 import textwrap
 from tabulate import tabulate
+from bson.decimal128 import Decimal128
 
 """
 TODO List:
@@ -19,7 +22,7 @@ def insert_db(db_host, db_name, col_name, docs_list, update_field=None):
     # for doc in docs_list:
         # col.update_one(filter={update_field : doc[update_field]}, update={"$set" : doc}, upsert=True)
 
-printer_doc = {
+machine_doc = {
     "_id" : "FDM 1",
     "brand" : "Prusa",
     "model" : "i3 MK3",
@@ -71,16 +74,21 @@ job_schema = {
         "shells",
         "infill",
         "top_bottom",
-        "notes"
+        "notes",
+        "status"
     ],
     "properties": {
         "name": {"bsonType": "string"},
         "email": {"bsonType": "string"},
-        "material": {"bsonType": "string"},
+        "material": {"bsonType": ["string", "objectId"]},
         "shells": {"bsonType": "int"},
         "infill" : {"bsonType": "int"},
         "top_bottom" : {"bsonType": "int"},
-        "notes": {"bsonType": "string"}
+        "notes": {"bsonType": "string"},
+        "status" : {"bsonType": "string"}, # inactive, queued, active, completed
+        "operator" : {"bsonType": ["string", "array"]},
+        "machine" : {"bsonType": ["string", "objectId"]},
+        "material_used" : {"bsonType": "int"},
     }
 }
 
@@ -91,7 +99,35 @@ job_doc = {
     "shells": 2,
     "infill": 20,
     "top_bottom": 4,
-    "notes": ""
+    "notes": "",
+    "status": "inactive",
+    "machine": machine_doc["_id"]
+}
+
+operator_schema = {
+    "title": "operator",
+    "required": [
+        "name",
+        "email",
+        "year",
+        "wage",
+        "hours"
+    ],
+    "properties": {
+        "name": {"bsonType": "string"},
+        "email": {"bsonType": "string"},
+        "year": {"bsonType": "string"},
+        "wage": {"bsonType": "number"},
+        "hours": {"bsonType": "number"}
+    }
+}
+
+operator_doc = {
+        "name": "John Smith",
+        "email": "test_operator@cooper.edu",
+        "year": "junior",
+        "wage": 15.75,
+        "hours": 2.5
 }
 
 def create_col_schema(db, col_name, schema):
@@ -103,9 +139,14 @@ def main():
         mongo_uri = f.readline().strip()
     client = pymongo.MongoClient(mongo_uri)
     db = client["makerspace"]
+    for col in db.list_collection_names():
+        db[col].drop()
     create_col_schema(db, "materials", material_schema)
     create_col_schema(db, "jobs", job_schema)
-
+    create_col_schema(db, "operators", operator_schema)
+    db["materials"].insert_one(material_doc)
+    db["jobs"].insert_one(job_doc)
+    db["operators"].insert_one(operator_doc)
 
 
 if __name__ == "__main__":

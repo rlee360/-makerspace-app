92cf55abcba95f375f9dd9c2c0d93d7a002307f1
rlee360 <Lee66@cooper.edu>
2021-12-14 05:31:43 -0500

    Began writing logic to display jobs; still need to implement draggable callbacks

--- makerspace-frontend/src/pages/operator/operator.vue
+++ makerspace-frontend/src/pages/operator/operator.vue
@@ -2,40 +2,32 @@
   <div class="container mt-5 mb-5">
     <h1>OPERATOR</h1>
     <div class="row">
-      <div class="col mx-2 px-2 py-3 bg-light border rounded" style ="max-height:300px">
+      <div class="col mx-2 px-2 py-3 bg-light border rounded" style="max-height:300px">
         <h6>New Jobs</h6>
-          <draggable class="draggable-list" :list="tasks.jobs" group="tasks">
-            <div v-for="(job, i) in tasks.jobs" :key="i">
-              <div class="bg-white mt-3 p-2 shadow border rounded">
-                <p>{{ job.title }}</p>
-              </div>
+        <draggable class="draggable-list" :list="tasks.jobs" group="tasks">
+          <div v-for="(job, i) in tasks.jobs" :key="i">
+            <div class="bg-white mt-3 p-2 shadow border rounded">
+              <p>{{ job.title }}</p>
             </div>
-          </draggable>
-        </div>
+          </div>
+        </draggable>
+      </div>
       <div class="col">
-        <div class="col mx-2 px-2 py-3 bg-light border rounded" style ="max-height:150px" >
-          <h6>Printer 1</h6>
-          <draggable class="draggable-list" :list="tasks.printer1" group="tasks">
-            <div v-for="(job, i) in tasks.printer1" :key="i">
-              <div class="bg-white mt-3 p-2 shadow border rounded">
-                <p>{{ job.title }}</p>
+        <div v-for="(mac, i) in machines" :key="i">
+          <div class="col mx-2 px-2 py-3 bg-light border rounded" style="max-height:20vh">
+            <h6>{{ mac._id }}</h6>
+            <draggable class="draggable-list" :list="tasks.printer1" group="tasks">
+              <div v-for="(job, i) in tasks.printer1" :key="i">
+                <div class="bg-white mt-3 p-2 shadow border rounded">
+                  <p>{{ job.title }}</p>
+                </div>
               </div>
-            </div>
-          </draggable>
-        </div>
-        <div class="col mx-2 px-2 py-3 bg-light border rounded" style ="max-height:150px">
-          <h6>Printer 2</h6>
-          <draggable class="draggable-list" :list="tasks.printer2" group="tasks">
-            <div v-for="(job, i) in tasks.printer2" :key="i">
-              <div class="bg-white mt-3 p-2 shadow border rounded">
-                <p>{{ job.title }}</p>
-              </div>
-            </div>
-          </draggable>
+            </draggable>
+          </div>
         </div>
       </div>
       <div class="col">
-        <div class="col mx-2 px-2 py-3 bg-light border rounded" style ="max-height:300px">
+        <div class="col mx-2 px-2 py-3 bg-light border rounded" style="max-height:300px">
           <h6>Completed</h6>
           <draggable class="draggable-list" :list="tasks.completed" group="tasks">
             <div v-for="(task, i) in tasks.completed" :key="i">
@@ -52,37 +44,30 @@
 
 <script>
 import draggable from "vuedraggable";
+import axios from "axios";
+
 export default {
   components: {
     draggable,
   },
   data() {
     return {
-      tasks: {
-        jobs: [  ],
-        printer1: [ ],
-        printer2: [ ],
-        completed: [ ],
-      },
+      incomplete_jobs : [],
+      machines: [],
+      completed_jobs :[]
     };
   },
-  mounted() {
-    fetch('http://localhost:3000/jobs')
-      .then(res => res.json())
-      .then(data => this.tasks.jobs = data)
-      .catch(err=> console.log(err.message))
-    fetch('http://localhost:3000/printer1')
-      .then(res => res.json())
-      .then(data => this.tasks.printer1 = data)
-      .catch(err=> console.log(err.message))
-    fetch('http://localhost:3000/printer2')
-      .then(res => res.json())
-      .then(data => this.tasks.printer2 = data)
-      .catch(err=> console.log(err.message))
-    fetch('http://localhost:3000/completed')
-      .then(res => res.json())
-      .then(data => this.tasks.completed = data)
-      .catch(err=> console.log(err.message))
+  async mounted() {
+    let job_query = {'status' : 'inactive'};
+    res = await axios.post("http://localhost:5000/api/request/filter", job_query, {'Content-Type': 'multipart/form-data'});
+    this.incomplete_jobs = res.data.filtered;
+    let res = await axios.get("http://localhost:5000/api/machine/?maintenance=False");
+    let m = res.data;
+    for (const el of m) {
+      res = await axios.get("http://localhost:5000/api/machine/?machine=" + el['_id']);
+      this.machines.push(res.data[0]);
+    }
+    window.ij = this.incomplete_jobs;
   }
 };
 </script>
@@ -91,13 +76,16 @@ export default {
 h6 {
   font-weight: 700;
 }
+
 .col {
   height: 90vh;
   overflow: auto;
 }
+
 .draggable-list {
   min-height: 10vh;
 }
+
 .draggable-list > div {
   cursor: pointer;
 }

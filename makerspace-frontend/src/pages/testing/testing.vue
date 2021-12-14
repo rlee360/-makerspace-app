ddebdd3537ff0723a70b2ad5de42d5d13981c090
rlee360 <Lee66@cooper.edu>
2021-12-13 21:10:10 -0500

    very broken testing.vue

--- makerspace-frontend/src/pages/testing/testing.vue
+++ makerspace-frontend/src/pages/testing/testing.vue
@@ -1,95 +1,51 @@
 <template>
   <div class="container mt-5 mb-5">
     <h1>Request Form</h1>
-    <FormulateForm class="inputs" @submit="onSubmit" v-model="values" @reset="onReset">
+    <FormulateForm class="inputs" @submit="onSubmit" v-model="values">
       <FormulateInput
-          name="file"
-          label="File"
-          type="Open File"
-          :state="Boolean(file1)"
-          placeholder="Choose a file or drop it here..."
-          drop-placeholder="Drop file here..."/>
-      <div class="mt-3">Selected file: {{ file1 ? file1.name : '' }}</div>
-
-      <FormulateInput
-          type="email"
-          name="email"
-          placeholder="Enter email"
-          label="Email address:"
-          validation="required|email"/>
-
-      <FormulateInput
-          name="material"
-          label="Material:"
-          type="select"
-          :options="{ material1: 'material1', material2:'material2'}"/>
-
-      <FormulateInput
-          name="shells"
-          label="Shells:"
-          type="number"
-          max="100"
-          min="0"/>
-
-      <FormulateInput
-          name="infill"
-          label="Infill:"
-          type="number"
-          max="100"
-          min="0"/>
-
-      <FormulateInput
-          name="top_bottom"
-          label="Top bottom:"
-          type="number"
-          max="100"
-          min="0"/>
-
-      <FormulateInput
-          name="Notes"
-          type="textarea"
-          label="Notes:"
-          placeholder="Please give a short description of the project"
-          rows="3"
-          max-rows="10"/>
+          type="file"
+          name="files"
+          label="Select your files to upload"
+          help="Select one or more docs to upload"
+      />
       <FormulateInput type="submit" value="Submit"/>
       <FormulateInput type="submit" value="Reset"/>
+      <pre>{{values}}</pre>
     </FormulateForm>
   </div>
 </template>
 
 <script>
-//import axios from "axios";
+import axios from "axios";
 
 export default {
   data() {
     return {
       requestData: {
-        files: {},
-        email: "",
-        name: "",
-        material: "",
-        notes: "",
-        shells: "",
-        infill: "",
-        top_bottom: "",
-        filename: ""
+        files: "",
+        email: "abc@cooper.edu",
+        name: "abc 123",
+        material: "PLA",
+        notes: "abb",
+        shells: "2",
+        infill: "30",
+        top_bottom: "3",
+        filename: "file.stl"
       }
     }
   },
   methods: {
     submitHandler() {
     },
-    async fileChanged(e1) {
-      console.log(e1);
-      //const res = await axios.get("http://localhost:5000/vuetest", {ttt: "data"});
-      //const res = await axios.get("http://localhost:8080");
-      //console.log(res);
-      //this.test = res.data["Status"];
+    async fileSet(f) {
+      console.log(f);
+      const res = await axios.post("http://localhost:5000/api/request/create", this.requestData, {'Content-Type': 'multipart/form-data'});
+      console.log(res);
     },
-    selectFile() {
-      console.log()
-      console.log("YO!", this.$refs.file.files);
+    async onSubmit() {
+      console.log(this.requestData);
+       const res = await axios.post("http://localhost:5000/api/request/create", this.requestData, {'Content-Type': 'multipart/form-data'});
+       console.log(res);
     }
   }
 }

f14a01bceb1388feb08a352b32bc82e54ad41df4
rlee360 <Lee66@cooper.edu>
2021-12-13 20:14:53 -0500

    Updated CSS, did some testing with POST requests

--- makerspace-frontend/src/pages/testing/testing.vue
+++ makerspace-frontend/src/pages/testing/testing.vue
@@ -1,29 +1,100 @@
 <template>
-  <!-- some code condensed for display reasons -->
-  <FormulateForm v-model="values" @submit="submitHandler">
-    <h2>Frosty's Frozen Treats</h2>
-    <FormulateInput
-      type="group"
-      name="flavors"
-      label="Create your custom order"
-      help="Choose your hand-packed pints whipped up by our expert servers"
-      add-label="+ Add Flavor"
-      validation="required"
-      :repeatable="true"
-    >
-      <div class="order">
-        <FormulateInput name="flavor" type="select" label="Flavor" validation="required" :options="{ vanilla: 'Vanilla', chocolate: 'Chocolate', strawberry: 'Strawberry', pineapple: 'Pineapple'}" />
-        <FormulateInput name="quantity" label="Quantity" type="number" min="1" validation="required|min:1" />
-      </div>
-    </FormulateInput>
+  <div class="container mt-5 mb-5">
+    <h1>Request Form</h1>
+    <FormulateForm class="inputs" @submit="onSubmit" v-model="values" @reset="onReset">
+      <FormulateInput
+          name="file"
+          label="File"
+          type="Open File"
+          :state="Boolean(file1)"
+          placeholder="Choose a file or drop it here..."
+          drop-placeholder="Drop file here..."/>
+      <div class="mt-3">Selected file: {{ file1 ? file1.name : '' }}</div>
 
-    <FormulateInput name="deliveryMethod" type="radio" label="Delivery Method" validation="required" :options="{ local: 'Local Pick-up', delivery: 'Delivery (5-mile radius)' }" />
-    <FormulateInput name="orderNotes" type="textarea" label="Order Notes" help="Allergies? Delivery instructions? Don't need the spoons? Let us know!" />
-    <FormulateInput type="submit" value="Place Order" />
-    <pre>{{ values }}</pre>
-  </FormulateForm>
+      <FormulateInput
+          type="email"
+          name="email"
+          placeholder="Enter email"
+          label="Email address:"
+          validation="required|email"/>
+
+      <FormulateInput
+          name="material"
+          label="Material:"
+          type="select"
+          :options="{ material1: 'material1', material2:'material2'}"/>
+
+      <FormulateInput
+          name="shells"
+          label="Shells:"
+          type="number"
+          max="100"
+          min="0"/>
+
+      <FormulateInput
+          name="infill"
+          label="Infill:"
+          type="number"
+          max="100"
+          min="0"/>
+
+      <FormulateInput
+          name="top_bottom"
+          label="Top bottom:"
+          type="number"
+          max="100"
+          min="0"/>
+
+      <FormulateInput
+          name="Notes"
+          type="textarea"
+          label="Notes:"
+          placeholder="Please give a short description of the project"
+          rows="3"
+          max-rows="10"/>
+      <FormulateInput type="submit" value="Submit"/>
+      <FormulateInput type="submit" value="Reset"/>
+    </FormulateForm>
+  </div>
 </template>
 
 <script>
-export default { data () { return { values: {} } }, methods: {submitHandler () { alert(`Thank you for your order!`) } } }
+//import axios from "axios";
+
+export default {
+  data() {
+    return {
+      requestData: {
+        files: {},
+        email: "",
+        name: "",
+        material: "",
+        notes: "",
+        shells: "",
+        infill: "",
+        top_bottom: "",
+        filename: ""
+      }
+    }
+  },
+  methods: {
+    submitHandler() {
+    },
+    async fileChanged(e1) {
+      console.log(e1);
+      //const res = await axios.get("http://localhost:5000/vuetest", {ttt: "data"});
+      //const res = await axios.get("http://localhost:8080");
+      //console.log(res);
+      //this.test = res.data["Status"];
+    },
+    selectFile() {
+      console.log()
+      console.log("YO!", this.$refs.file.files);
+    }
+  }
+}
 </script>
+
+<style lang="scss">
+@import 'node_modules/@braid/vue-formulate/themes/snow/snow.scss';
+</style>
